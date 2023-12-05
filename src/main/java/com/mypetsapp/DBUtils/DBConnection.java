33cd3b455251a493eb93702775df68c983e20503
DiegoAlbarracin <diegoalbarracin0405@gmail.com>
2023-12-05 18:10:30 -0500

    Updating the readme.md file.

--- src/main/java/com/mypetsapp/DBUtils/DBConnection.java
+++ src/main/java/com/mypetsapp/DBUtils/DBConnection.java
@@ -6,18 +6,18 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class DBConnection {
-    public static String userName;
-    public static String passWord;
+    public static String userName = "is358200";
+    public static String passWord = "366njk69m5RC";
 
     public static String urlDB = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521/LAB";
     public static Connection connect() {
         Scanner sc = new Scanner(System.in);
         Connection conn = null;
-        System.out.println("Conexion a la Base de Datos");
-        System.out.print("Usuario ->");
-        userName = sc.nextLine();
-        System.out.print("Contrasena ->");
-        passWord = sc.nextLine();
+//        System.out.println("Conexion a la Base de Datos");
+//        System.out.print("Usuario ->");
+//        userName = sc.nextLine();
+//        System.out.print("Contrasena ->");
+//        passWord = sc.nextLine();
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             conn = DriverManager.getConnection(urlDB, userName, passWord);
